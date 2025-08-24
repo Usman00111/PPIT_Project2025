2b402b249227a6fb0796827afff0d1a97eb2ca82
Usman00111 <123650130+Usman00111@users.noreply.github.com>
2025-08-24 21:14:26 +0000

    added run() skeleton with DB connect/close

--- backend/scripts/seedProducts.js
+++ backend/scripts/seedProducts.js
@@ -2,3 +2,10 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 const connectDB = require("../config/db");
 const Product = require("../models/Product");
+
+async function run() {
+  await connectDB();
+  // seeding will go here
+  await mongoose.connection.close();
+  process.exit(0);
+}
