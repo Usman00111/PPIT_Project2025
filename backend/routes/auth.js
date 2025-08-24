5b2855f340e47599b38f5121623dff1a65b7deb0
Usman00111 <123650130+Usman00111@users.noreply.github.com>
2025-08-24 20:31:38 +0000

    added duplicate email check

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -13,6 +13,8 @@ router.post("/register", async (req, res) => {
     } catch (e) {
         return res.status(500).json({ message: e.message });
     }
+    const exists = await User.findOne({ email });
+    if (exists) return res.status(400).json({ message: "Email already in use." });
 });
 
 
