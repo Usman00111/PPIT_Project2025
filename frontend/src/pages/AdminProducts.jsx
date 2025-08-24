93f2ac0abd4dbf2c224034340cf18d4101ad20e2
Usman00111 <123650130+Usman00111@users.noreply.github.com>
2025-08-24 16:18:36 +0000

    imports for admin products with load products into state on mount using useState and useEffect

--- frontend/src/pages/AdminProducts.jsx
+++ frontend/src/pages/AdminProducts.jsx
@@ -1,4 +1,9 @@
-export default function AdminProducts() {
-    return (<div><h2>Admin: Products</h2>
-    </div>);
-}
+import { useEffect, useState } from "react";
+import { getProducts } from "../lib/productStore";
+
+export default function AdminProducts(){
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    setList(getProducts());
+  }, []);
