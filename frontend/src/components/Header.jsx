5a886050e5b07ed893d8f0d2dcf1504cfef0e446
Usman00111 <123650130+Usman00111@users.noreply.github.com>
2025-08-24 15:28:14 +0000

    import useAuth

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,5 +1,6 @@
 import { Link, useNavigate } from "react-router-dom";
 import { useCart } from "../context/CartContext";
+import { useAuth } from "../context/AuthContext";
 
 export default function Header() {
     const navigate = useNavigate();
