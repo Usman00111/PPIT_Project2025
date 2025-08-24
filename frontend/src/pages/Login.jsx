71fca876619501ba916ad863873dd38939bb3688
Usman00111 <123650130+Usman00111@users.noreply.github.com>
2025-08-24 15:32:44 +0000

    implementing Login page using loginStub and navigate to home

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -4,6 +4,17 @@ import { useNavigate, Link } from "react-router-dom";
 
 
 export default function Login() {
-    return (<div><h2>Login</h2>
-    </div>);
+    const nav = useNavigate();
+    const { loginStub } = useAuth();
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState(""); // not used yet, kept for UI purposess
+
+    function onSubmit(e) {
+        e.preventDefault();
+        if (!email) return;
+        loginStub(email);
+        nav("/");
+
+    }
+
 }
