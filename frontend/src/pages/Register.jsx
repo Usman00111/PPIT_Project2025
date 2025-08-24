e88f89914f59c96ae6243a212cfc8d3f17a9f2ce
Usman00111 <123650130+Usman00111@users.noreply.github.com>
2025-08-24 15:34:28 +0000

    imports for register.jsx

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,3 +1,7 @@
+import { useState } from "react";
+import { useAuth } from "../context/AuthContext";
+import { useNavigate, Link } from "react-router-dom";
+
 export default function Register() {
     return (<div><h2>Register</h2>
     </div>);
