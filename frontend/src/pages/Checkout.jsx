6253201d676591163b1bd45eb8e7e1af803b39e6
Usman00111 <123650130+Usman00111@users.noreply.github.com>
2025-08-24 15:38:49 +0000

    add Checkout component skeleton with bsic headings

--- frontend/src/pages/Checkout.jsx
+++ frontend/src/pages/Checkout.jsx
@@ -2,7 +2,13 @@ import { useState } from "react";
 import { useAuth } from "../context/AuthContext";
 import { useCart } from "../context/CartContext";
 
-export default function Checkout() {
-    return (<div><h2>Checkout</h2>
-    </div>);
+export default function Checkout(){
+  const { user } = useAuth();
+  const { items, total, clearCart } = useCart();
+
+  return (
+    <div>
+      <h2>Checkout</h2>
+    </div>
+  );
 }
