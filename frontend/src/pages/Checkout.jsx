b01c49f4572167d76688b43960628a4b589640c9
Usman00111 <123650130+Usman00111@users.noreply.github.com>
2025-08-24 15:39:38 +0000

    added guest form state variables

--- frontend/src/pages/Checkout.jsx
+++ frontend/src/pages/Checkout.jsx
@@ -6,6 +6,14 @@ export default function Checkout(){
   const { user } = useAuth();
   const { items, total, clearCart } = useCart();
 
+    // guest fields
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [address, setAddress] = useState("");
+  const [eircode, setEircode] = useState("");
+
+
   return (
     <div>
       <h2>Checkout</h2>
