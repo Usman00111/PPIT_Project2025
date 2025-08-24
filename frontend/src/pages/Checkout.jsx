cb41bf0cc6b63c305ea41bbced8bdd1087f0f269
Usman00111 <123650130+Usman00111@users.noreply.github.com>
2025-08-24 15:37:31 +0000

    imports for checkout page.jsx

--- frontend/src/pages/Checkout.jsx
+++ frontend/src/pages/Checkout.jsx
@@ -1,3 +1,7 @@
+import { useState } from "react";
+import { useAuth } from "../context/AuthContext";
+import { useCart } from "../context/CartContext";
+
 export default function Checkout() {
     return (<div><h2>Checkout</h2>
     </div>);
